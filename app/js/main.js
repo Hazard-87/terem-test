57810755a996b392bdf51820500b16f697b9c6f4
Hazard <dpetryshkin@gmail.com>
2021-04-25 01:38:09 +0300

    Кое какие правки

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
-  (function helloWorld() {
+  (() => {
     alert('hello, world!!!');
   })();
-  // helloWorld();
 
-  // const button1 = document.querySelector('#button1');
-  // button2 = document.querySelector('#button2');
-  // const block1 = document.querySelector('#block1');
+  // const button1 = document.querySelector('#button1'),
+  // button2 = document.querySelector('#button2'),
+  // block1 = document.querySelector('#block1');
   const blocks = document.querySelectorAll('.inner .col-sm');
 
   function toggle(block, className) {
