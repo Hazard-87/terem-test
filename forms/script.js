7051248b87b087158a4a8e93d11b4937d631230a
Hazard <dpetryshkin@gmail.com>
2021-04-26 15:14:42 +0300

    Исправления в script.js

--- forms/script.js
+++ forms/script.js
@@ -1,18 +1,6 @@
 const form = document.querySelector('#form'),
   field = document.querySelector('.forms-json');
 
-const postData = async (url, data) => {
-  const res = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: data,
-  });
-
-  return await res.text(); // надо переформатировать данные для возврата
-};
-
 const forms = document.querySelectorAll('form');
 
 forms.forEach((item) => {
@@ -24,19 +12,23 @@ function bindPostData(form) {
     event.preventDefault();
 
     const formData = new FormData(form);
-    const json = JSON.stringify(Object.fromEntries(formData.entries()));
+    const data = JSON.stringify(Object.fromEntries(formData.entries()));
 
     field.textContent = `
-        ${json}
+        ${data}
         `;
 
-    postData('server.php', json)
-      .then((data) => {
-        form.reset();
-        console.log(data);
-      })
-      .catch(() => {
-        alert('Возникла какая-то ошибка...');
+    fetch('server.php', {
+      method: 'POST',
+      body: data,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.text())
+      .then((data) => alert(data))
+      .catch((e) => {
+        console.log(e.message);
       });
   });
 }
